c1639c8acd93c4b3424e86c3d4c8c6e263433c47
delvedor <tommydelved@gmail.com>
2021-03-11 17:30:15 +0100

    Updated test

---
+++ test/unit/serializer.test.ts
@@ -0,0 +1,51 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { test } from 'tap'
+import qs from 'qs'
+import Serializer from '../../lib/Serializer'
+
+test('qserialize', t => {
+  t.plan(1)
+  const s = new Serializer()
+  const obj = {
+    hello: 'world',
+    you_know: 'for search'
+  }
+
+  t.strictEqual(
+    s.qserialize(obj),
+    qs.stringify(obj)
+  )
+})
+
+test('qserialize array', t => {
+  t.plan(1)
+  const s = new Serializer()
+  const obj = {
+    foo: 'bar',
+    baz: [1, 2, 3]
+  }
+
+  t.strictEqual(
+    s.qserialize(obj),
+    'foo=bar&baz%5B%5D=1&baz%5B%5D=2&baz%5B%5D=3'
+  )
+})
+
